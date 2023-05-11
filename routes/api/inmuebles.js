a401282578c23a1c60f4838554e6757d41958233
Gzar81 <116944846+Gzar81@users.noreply.github.com>
2023-05-12 01:35:26 +0200

    mejorados métodos y mensajes

--- routes/api/inmuebles.js
+++ routes/api/inmuebles.js
@@ -8,7 +8,7 @@ const Inmueble = require('../../models/inmueble.model');
 router.get('/', async (req, res) => {
     try {
         const inmuebles = await Inmueble.find();
-        res.json(inmuebles);
+        inmuebles.length === 0 ? res.json("No existen registros de inmuebles en la base de datos") : res.json(inmuebles);
     } catch (error) {
         res.json({ fatal: error.message });
     }
@@ -18,12 +18,33 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
     try {
-        const result = await Inmueble.create(req.body);
-        res.json(result);
+        // Crea un objeto con los campos de búsqueda que te interesan.
+        const searchCriteria = {
+            piso: req.body.piso,
+            letra: req.body.letra,
+            extension: req.body.extension,
+            numHabitaciones: req.body.numHabitaciones,
+            nombrePropietario: req.body.nombrePropietario,
+            emailContacto: req.body.emailContacto
+        };
+
+        // Busca si el inmueble ya existe en la base de datos.
+        const inmuebleExistente = await Inmueble.findOne(searchCriteria);
+
+        if (inmuebleExistente) {
+            // Si el inmueble ya existe, envía un mensaje indicándolo.
+            res.json({ message: "Ya existe un inmueble con los mismos datos, revíselo para evitar duplicidades" });
+        } else {
+            // Si el inmueble no existe, crea uno nuevo.
+            const result = await Inmueble.create(req.body);
+            res.json(result);
+        }
     } catch (error) {
         res.json({ fatal: error.message });
     }
 });
+
+
 
 // ACTUALIZAR LOS DATOS DE UN INMUEBLE
 
@@ -31,14 +52,17 @@ router.put('/:inmuebleId', async (req, res) => {
     const { inmuebleId } = req.params;
 
     try {
-        const result = await Inmueble.findByIdAndUpdate(
-            inmuebleId, req.body, { new: true }
-        );
+        const inmueble = await Inmueble.findById(inmuebleId);
+        if (!inmueble) {
+            return res.status(404).json({ message: "El id del inmueble que usted quiere actualizar no existe" });
+        }
+        const result = await Inmueble.findByIdAndUpdate(inmuebleId, req.body, { new: true });
         res.json(result);
     } catch (error) {
         res.json({ fatal: error.message });
     }
 });
+
 
 // BORRAR LOS DATOS DE UN INMUEBLE
 
